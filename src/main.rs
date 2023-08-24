84379242f7587cdbb38e716ff776b2717590c0c9
Ray <zhuoxun.yang777@outlook.com>
2023-08-24 21:16:34 +0800

    init one thread

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,33 @@
 use std::{
     fs,
-    io::{prelude::*, BufReader},
+    io::prelude::*,
     net::{TcpListener, TcpStream},
 };
-use webserver::ThreadPool;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").expect("Can't bind port 7878");
-    let pool = ThreadPool::new(5);
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
-        let stream = match stream {
-            Ok(stream) => stream,
-            Err(_) => break,
-        };
+        let stream = stream.unwrap();
 
-        pool.execute(|| {
-            handle_connection(stream);
-        });
+        handle_connection(stream);
     }
-
-    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
 
-    let request_line = match buf_reader.lines().next().unwrap() {
-        Ok(request_line) => request_line,
-        Err(error) => {
-            println!("Error: {:?}", error);
-            return;
-        }
-    };
+    let get = b"GET / HTTP/1.1\r\n";
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     };
-
     let contents = fs::read_to_string(filename).unwrap();
-    let length = contents.len();
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
+    let response = format!("{status_line}{contents}");
     stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
